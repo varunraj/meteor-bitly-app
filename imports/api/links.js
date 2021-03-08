ec7a0ef11d033e8f48ef050b133a6b5bee4e869c
Varun Raj <varunraj82@gmail.com>
2021-03-08 09:29:22 -0500

    updated

--- imports/api/links.js
+++ imports/api/links.js
@@ -8,7 +8,7 @@ Meteor.methods({
     'links.insert': function (url) {  // methods to be executed when inserting a link. We can call links.insert and this method will be executed
         //console.log('attempting to save url', url)
         //validUrl.isUri(url)
-        check(url, Match.Where(url => validUrl.isUri(url)))// Match is used to find it is a truthy value or not.
+        check(url, Match.Where(url => validUrl.isUri(url)))// Match is used to find it is a valid url.
 
 
     }
